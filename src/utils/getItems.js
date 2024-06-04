08cbb8911be64dfd8efe402266957d6cd2e39c01
Goutam Kumar <tkgoutamm625@gmail.com>
2024-06-04 10:06:59 +0530

    update src/utils/getItems.js

--- src/utils/getItems.js
+++ src/utils/getItems.js
@@ -1,5 +1,5 @@
 const getItems = async () => {
-  const res = await fetch('http://localhost:3000/api/files', { cache: 'no-store' });
+  const res = await fetch('/api/files', { cache: 'no-store' });
   const { items } = await res.json();
   return items;
 }
